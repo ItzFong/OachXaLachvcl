9c2437dfa0e5d03e61856a19d603274cbec07cfc
ItzFong <104250326+ItzFong@users.noreply.github.com>
2023-06-02 10:39:32 +0700

    New code

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("Hello world")
+import shutil
+import psutil
+def check_free_disk(disk):
+    du = shutil.disk_usage(disk)
+    free = (du.free/du.total)*100
+    return free>20
+def check_cpu_percent(): 
+    usage = psutil.cpu_percent(1)
+    return usage
+if not check_free_disk("/") or not check_cpu_percent():
+    print("ERROR!")
+else: 
+    print("EVERYTHING IS OK!")
